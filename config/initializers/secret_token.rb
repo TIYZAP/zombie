c0234063341b8e0250bf75142861fca807894333
TIYZAP <ironyardzap@gmail.com>
2017-02-19 19:54:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zombie::Application.config.secret_token = '82677c3f42a3e94011bbc86066d64f96ccd844d689a41484bc6a91bb90bd254f329a3856226e7a2afdee2b12d1f44c4040fc18b180d3cc2e89fc7a1907078fb2'
